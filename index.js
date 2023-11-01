1f31f803874a14f89e11d3152971140416598809
jay <jaymawari31@gmail.com>
2023-11-01 13:46:54 +0530

    adding admin folder

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const app=express();
+const path=require('path');
+const port=8000
+
+
+app.use(cors());
+app.use(bodyParser.json())
+
+app.get("/",(req,res)=>{
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get("/admin",(req,res)=>{
+  res.sendFile(path.join(__dirname + '/admin.html'))
+})
+
+app.get("/delete",(req,res)=>{
+  res.sendFile(path.join(__dirname + '/data.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Server is successfully running on http://localhost:${port}`);
+})
